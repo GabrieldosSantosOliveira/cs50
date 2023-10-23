439d62a0b74b6af4cb65347cb97d5ea73ad74b71
Gabriel dos Santos Oliveira <gabrielsantosoliveira951@gmail.com>
2023-10-22 20:28:11 -0400

    feat: add resolution challenge mario easy version

---
+++ mario_easy_version/main.c
@@ -0,0 +1,29 @@
+//
+// Created by gabriel on 22/10/23.
+//
+#include <stdio.h>
+
+int getSize() {
+    int size;
+    do {
+        printf("Tamanho: ");
+        scanf("%d", &size);
+    } while (size < 1 || size > 8);
+    return size;
+}
+
+int main() {
+    int size = getSize();
+    for (int i = 0; i < size; i++) {
+        for (int k = 0; k < size; k++) {
+            int emptySpace = ((i + 1) - size) * -1;
+            if (k < emptySpace) {
+                printf(" ");
+            } else {
+                printf("#");
+            }
+        }
+        printf("\n");
+    }
+    return 0;
+}
